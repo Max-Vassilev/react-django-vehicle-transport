b95a6244d0378efacf372fee7157791136e998de
Max Vassilev <maximvassilev01@gmail.com>
2025-05-26 12:38:50 +0300

    Remove not needed dependancies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,13 +39,11 @@ INSTALLED_APPS = [
     "backend.api",
     "rest_framework",
     "corsheaders",
-    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -100,7 +98,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
