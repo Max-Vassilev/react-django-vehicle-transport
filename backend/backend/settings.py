f1fb98127ddbc1659fc1bb58ff09b00d53ec7f37
Max Vassilev <maximvassilev01@gmail.com>
2025-06-01 20:10:08 +0300

    Use public IP of the EC2 instance instead of localhost in the links

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,11 +9,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-]
-
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
--- frontend/src/components/Buttons.jsx
+++ frontend/src/components/Buttons.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Buttons = () => {
   return (
     <div className="buttons-container">
-      <a href="http://localhost:5173/login/" className="button login">Login</a>
-      <a href="http://localhost:5173/register/" className="button register">Register</a>
-      <a href="http://localhost:5173/logout/" className="button logout">Logout</a>
+      <a href="http://23.22.149.131:5173/login/" className="button login">Login</a>
+      <a href="http://23.22.149.131:5173/register/" className="button register">Register</a>
+      <a href="http://23.22.149.131:5173/logout/" className="button logout">Logout</a>
     </div>
   );
 }
