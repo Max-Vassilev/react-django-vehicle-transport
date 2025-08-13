eb99f2659a8dc8b4009c4a554deb0a923794f319
Maxim Vassilev <maximvassilev01@gmail.com>
2025-08-13 17:21:20 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['vehicle-transport-alb-1148766537.us-east-1.elb.amazonaws.com']
+    allowedHosts: ['vehicle-transport-alb-1148766537.us-east-1.elb.amazonaws.com'],
+    hmr: {
+      host: 'vehicle-transport-alb-1148766537.us-east-1.elb.amazonaws.com',
+      protocol: 'ws',
+    }
   }
 })
